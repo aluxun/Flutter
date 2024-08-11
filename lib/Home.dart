bff2c347f5e4d4c4118f58a1ede29deafa0865a4
wst <813998927@qq.com>
2024-08-11 11:54:10 +0800

    first_commit_flutter

---
+++ lib/Home.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+class HomeForm extends StatefulWidget{
+  const HomeForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() =>_HomeState();
+
+}
+
+class _HomeState extends State<HomeForm>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(appBar: AppBar
+       (
+        title:  Text('你好flutter'),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+        actions: [IconButton(onPressed: ()=>print("IconButton"), icon:Icon(Icons.search) ) ]
+        ),
+        body:Center(child:Container(
+
+               child: TextField(maxLines: 5),
+        )
+            ),
+        backgroundColor: Colors.red,
+        drawer:Drawer(child:ListView(children: [ListTile(title: Text('用户设置')),ListTile(title: Text('用户设置')),ListTile(title: Text('用户设置'))],) )
+
+    );
+  }
+
+}
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_demo/Home.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home:HomeForm()));
+}
+
+
+
+
+
+
+
+
+
+
+
+
